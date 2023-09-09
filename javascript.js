04248e718021b523e81c6c32021b749d32878f07
ooexiaoo <vpaherwar@gmail.com>
2023-09-09 15:28:16 +0530

    09-09-2023 Updated Js serving random iamge locally

---
+++ javascript.js
@@ -0,0 +1,37 @@
+// Mobile menu toggle
+window.onload = function () {
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
+}
+
+// Background image randomization
+document.addEventListener("DOMContentLoaded", function () {
+    function pic() {
+        var bgm = [
+            '/images/img1.webp',
+            '/images/img2.webp',
+            '/images/img3.webp',
+            '/images/img4.webp',
+            '/images/img5.webp',
+            '/images/img6.webp',
+            '/images/img7.webp',
+            '/images/img8.webp',
+        ];
+
+        // Get a random index from the array
+        var randomIndex = Math.floor(Math.random() * bgm.length);
+
+        // Set the background image of the body
+        document.body.style.background = 'url(' + bgm[randomIndex] + ') no-repeat';
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundAttachment = 'fixed';
+        document.body.style.backgroundColor = 'black';
+    }
+
+    pic();
+});
